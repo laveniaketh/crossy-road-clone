837fb46b135b99031a02442b86874ba643f5de5b
laveniaketh <laveniaketh@gmail.com>
2026-03-05 11:36:24 +0800

    adds the car and the truck model

---
+++ src/metadata.ts
@@ -0,0 +1,16 @@
+import type { Row } from "./types";
+
+export const rows: Row[] = [
+  {
+    // type: "forest",
+    // trees: [
+    //   { tileIndex: -3, height: 50 },
+    //   { tileIndex: 2, height: 30 },
+    //   { tileIndex: 5, height: 50 },
+    // ],
+    type: "car",
+    direction: true,
+    speed: 1,
+    vehicles: [{ initialTileIndex: 2, color: 0xff0000 }],
+  },
+];
